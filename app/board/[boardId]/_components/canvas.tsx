fc731b572d988761b420ec6c08c0e4b431c2e96c
belief2002 <satyampatel30smart@gmail.com>
2024-05-14 19:13:33 +0530

    color change & deletion(color not added)

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -34,6 +34,7 @@ import {
 import { LiveObject } from "@liveblocks/client";
 import { LayerPreview } from "./layer-preview";
 import { SelectionBox } from "./selection-box";
+import { SelectionTools } from "./selection-tools";
 
 const MAX_LAYERS = 100;
 interface CanvasProps {
@@ -273,6 +274,11 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         canUndo={canUndo}
         undo={history.undo}
         redo={history.redo}
+      />
+
+      <SelectionTools
+      camera={camera}
+      setLastUsedColor={setLastUsedColor}
       />
 
       <svg
